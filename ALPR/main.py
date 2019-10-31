83d7344effa572e1c5c63ce3b91b2b3d3f0ea91c
oscarlaureano <oscar.laureano.xx@gmail.com>
2019-10-31 14:02:08 -0600

    main.py fixed

--- ALPR/main.py
+++ ALPR/main.py
@@ -2,21 +2,24 @@ import sys
 import cv2
 import json
 import boto3
+import os
 from botocore.exceptions import NoCredentialsError
 from alvr import detectPlate
+from datetime import datetime, timezone
 
-ACCESS_KEY = 'AKIA2NEDLSIHWVJLPJVW'
-SECRET_KEY = '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'
+ACCESS_KEY = 'X'
+SECRET_KEY = 'X'
 
-path = 'C:/Users/oscar/Desktop/accenture-parking-solution/DB/'
+path = '../DB/'
 pk = 0
 
 def uploadToAws(local_file, bucket, s3_file = None):
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
                       aws_secret_access_key=SECRET_KEY)
-
+    
+    content_type = 'image/png' if local_file.split('.')[1] == 'png' else 'text/plain'
     try:
-        s3.upload_file(local_file, bucket, s3_file)
+        s3.upload_file(local_file, bucket, s3_file, ExtraArgs={'ACL': 'public-read', 'Metadata': {'Content-Type': content_type}})
         # print("Upload Successful")
         return True
     except FileNotFoundError:
@@ -27,26 +30,36 @@ def uploadToAws(local_file, bucket, s3_file = None):
         return False
 
 
-def action(path):
+def action(image_path):
     global pk
     
     # Detecting and processing image
-    text, cropped, img = detectPlate(path)
+    text, cropped, img = detectPlate(image_path)
 
     # Save files locally
     nameFull = str(pk) + '-full.png'
-    cv2.imwrite(path + nameFull, img)
+    if not cv2.imwrite(os.path.join(path, nameFull), img):
+        raise Exception("Could not write image")
+
 
     nameCropped = str(pk) + '-cropped.png'
-    cv2.imwrite(path + nameCropped, cropped)
+    if not cv2.imwrite(os.path.join(path, nameCropped), cropped):
+        raise Exception("Could not write image")
 
     nameText = str(pk) + '-text.json'
     data = {
-        'plate' : text
+        'plate' : text,
+        'created_on': datetime.now().strftime('%d %b %Y')
     }
-    with open(path + nameText, 'w') as fp:
+    with open(os.path.join(path, nameText), 'w') as fp:
         json.dump(data, fp)
     
+    # print(data)
+    # cv2.imshow('image',img)
+    # cv2.imshow('Cropped',cropped)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
     # Upload things to bucket and delete local files
     uploadToAws(path + nameFull, 'accenture-parking-solution', nameFull)
     uploadToAws(path + nameCropped, 'accenture-parking-solution', nameCropped)
